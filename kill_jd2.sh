232c1ebb1b7c5057e8764171fb2cd4127b829438
tknr <geishagirlskoume@gmail.com>
2021-01-10 11:48:36 +0900

    2021年  1月 10日 日曜日 11:48:36 JST

---
+++ kill_jd2.sh
@@ -0,0 +1,3 @@
+#!/bin/bash -x
+kill -9 `cat /opt/jd2/JDownloader.pid `
+rm -f /opt/jd2/JD2.lock
--- open_proxy_txt_with_geany.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -x
-/bin/nice -n 19 /bin/bash -x /opt/jd2/proxy/proxy.sh
-/bin/nice -n 19 /usr/bin/geany /opt/jd2/proxy/proxy.txt
