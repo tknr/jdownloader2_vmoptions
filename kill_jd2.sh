90bf4e3e2f06c142f8fdf06b80c2a593c189d294
tknr <geishagirlskoume@gmail.com>
2024-12-06 14:55:03 +0900

    u

--- kill_jd2.sh
+++ kill_jd2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -x
-find /opt/jd2/ -iname "*.log" -delete
-kill -9 `cat /opt/jd2/JDownloader.pid `
-rm -f /opt/jd2/JD2.lock
+find ../ -iname "*.log" -delete
+kill -9 `cat ../JDownloader.pid `
+rm -f ../JD2.lock
